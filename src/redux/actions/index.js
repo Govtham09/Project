39e2c65a6ac772eef5f983b5e60aa04aa9f3b328
Govtham09 <83535662+Govtham09@users.noreply.github.com>
2023-12-06 17:21:18 +0530

    Add files via upload

---
+++ src/redux/actions/index.js
@@ -0,0 +1,15 @@
+//For Adding Item to Cart
+export const addCart = (product) => {
+  return {
+    type: "ADDITEM",
+    payload: product,
+  };
+};
+
+//For Delete Item from Cart
+export const delCart = (product) => {
+  return {
+    type: "DELITEM",
+    payload: product,
+  };
+};
